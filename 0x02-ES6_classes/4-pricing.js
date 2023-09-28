81175d339748b5715e4d80048ccc1f4aaf5d1d91
bmn44100 <njerumurimi@gmail.com>
2023-09-28 12:01:11 -0700

    Implement a class named Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(attr) {
+    this._amount = attr;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(attr) {
+    this._currency = attr;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
