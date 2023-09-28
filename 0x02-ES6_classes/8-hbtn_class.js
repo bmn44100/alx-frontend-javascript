886d4135039d8ba99d854f2db62f342dd4974116
bmn44100 <njerumurimi@gmail.com>
2023-09-28 12:13:20 -0700

    Implement a class named HolbertonClass

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,18 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](attr) {
+    if (attr === 'number') {
+      return this._size;
+    }
+
+    if (attr === 'string') {
+      return this._location;
+    }
+
+    return null;
+  }
+}
